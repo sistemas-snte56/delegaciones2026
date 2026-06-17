f9f979547e8628222e56c0a54d40ecda598b2131
sistemas-snte56 <linux.vic@gmail.com>
2026-06-17 15:26:00 -0600

    feat: Validacion de clave duplicada en DelegacionObserver

--- app/Observers/DelegacionObserver.php
+++ app/Observers/DelegacionObserver.php
@@ -11,7 +11,21 @@ class DelegacionObserver
      */
     public function creating(Delegacion $delegacion): void
     {
-        $delegacion->clave_delegacion = $this->generarClave($delegacion);
+        // $delegacion->clave_delegacion = $this->generarClave($delegacion);
+
+        // Generamos la clave antes de guardar para verificar si ya existe una delegación con la misma clave
+        $clave = $this->generarClave($delegacion);
+
+        // Busca si ya existe una delegación con la misma clave en la base de datos
+        $existeDelegacion = Delegacion::where('clave_delegacion', $clave)->exists();
+
+        // Si ya existe una delegación con la misma clave, lanzamos una excepción para evitar la creación
+        if ($existeDelegacion) {
+            throw new \Exception("Ya existe una delegación con la clave '{$clave}'.");
+        }
+
+        // Si no existe, asignamos la clave generada a la delegación y permitimos que se guarde
+        $delegacion->clave_delegacion = $clave;
     }
     
     public function updating(Delegacion $delegacion): void
